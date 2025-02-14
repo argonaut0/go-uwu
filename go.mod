e88b1190a4574cc1ab7d3c5f5686890c3ab1abd2
Jason Hsu <jasonhsu2013@gmail.com>
2025-02-13 22:43:19 -0500

    Nothing weird here

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/argonaut0/go-uwu
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+
+import "fmt"
+
+func main() {
+	fmt.Println("totally safe! :) No weird stuff here!")
+	fmt.Println("Good for you! You audited the source code!")
+}
